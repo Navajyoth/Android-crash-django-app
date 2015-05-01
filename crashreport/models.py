4c213fa5dfb9a821b6e8f0eaff4c804f68185f1e
farhan <farhan.latheef1@gmail.com>
2015-05-01 15:19:13 +0530

    more models

--- crashreport/models.py
+++ crashreport/models.py
@@ -8,6 +8,12 @@ class crashReport(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     android_ver = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
+    log_cat = models.TextField(null=True)
+    device_model = models.CharField(max_length=100, null=True)
+    app_version_name = models.CharField(max_length=100, null=True)
+    app_verson_code = models.CharField(max_length=100, null=True) 
+    app_package = models.CharField(max_length=100, null=True)
+    product = models.CharField(max_length=100, null=True)
 
     def __str__(self):
     	return str(self.date)
--- crashreport/views.py
+++ crashreport/views.py
@@ -9,7 +9,15 @@ def crash(request):
     crashreport = request.POST.get('STACK_TRACE')
     android_ver = request.POST.get('ANDROID_VERSION')
     brand = request.POST.get('BRAND')
-    print (crashreport)
-    cr = crashReport(crash_report=crashreport, android_ver=android_ver, brand=brand)
+
+    log_cat = request.POST.get('LOGCAT')
+    model = request.POST.get('PHONE_MODEL')
+    app_version_name = request.POST.get('APP_VERSION_NAME')
+    app_verson_code = request.POST.get('APP_VERSION_CODE')
+    app_package = request.POST.get('PACKAGE_NAME')
+    product = request.POST.get('PRODUCT')
+
+    # print (request.POST)
+    cr = crashReport(crash_report=crashreport, android_ver=android_ver, brand=brand, log_cat=log_cat, device_model=model, app_version_name=app_version_name, app_verson_code=app_verson_code, product=product, app_package = app_package)
     cr.save()
     return 1
