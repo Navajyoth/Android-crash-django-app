575a71490c3205e92dac8aba897bb1b0e7b5392d
farhan <farhan.latheef1@gmail.com>
2015-05-04 17:00:54 +0530

    add version code to list name

--- crashreport/admin.py
+++ crashreport/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import crashReport
 # Register your models here.
 
+@admin.register(crashReport)
+class ReportAdmin(admin.ModelAdmin):
+	list_display = ('app_verson_code', 'device_model', 'crash_date', )
+	list_filter = ['app_verson_code', 'device_model', 'crash_date', ]
 
-admin.site.register(crashReport)
+# admin.site.register(crashReport)
